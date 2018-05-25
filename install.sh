a7c8faac6509d85e95735aadc5b1ea60e021ca0f
EMH-Mark-I <EMH_Mark_I@protonmail.com>
2018-05-24 23:23:24 -0500

    Fixes

--- install.sh
+++ install.sh
@@ -2,21 +2,17 @@
 
 read -s -n 1 -p "Press any key to begin..."
 
-#Get location
-path=`realpath $0`
-installdir=`dirname $path`
-
-if [ -d "$installdir/ytscrape_contents/" ];
+if [ -d "ytscrape_contents/" ];
 then
 	echo ""
-	echo "Removing $installdir/ytscrape_contents/"
-	rm -rf $installdir/ytscrape_contents/
+	echo "Removing ytscrape_contents/"
+	rm -rf ytscrape_contents/
 fi
 
 	echo ""
 	echo "Installing ytscrape"
-	mkdir -p script/ytscrape_contents/ &&
-	cd script/ytscrape_contents/ &&
+	mkdir -p ytscrape_contents/ &&
+	cd ytscrape_contents/ &&
 
 	#Check CPU type
 	if [ `getconf LONG_BIT` = "64" ]
@@ -29,11 +25,11 @@ fi
 	unzip pup_v0.4.0_linux_* &&
 	mkdir -p ~/bin/ &&
 	cp pup ~/bin &&
-	cd ../.. &&
+	cd .. &&
 	cp -R ytscrape/* ~/bin &&
-	rm -rf script/ytscrape_contents/
+	rm -rf ytscrape_contents/
 	echo ""
 	echo "Don't forget to install mpv player and youtube-dl!"
 	read -s -n 1 -p "Install complete. Press any key to exit..."
-
+	clear
 exit 0
